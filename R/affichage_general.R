474e88413ad7d5b0ba4bd309ee43f3a155b86a6d
Juliette Chiapello <chiapellojuliette@gmail.com>
2021-07-23 15:29:23 +0200

    couleurs histogramme corrigées

--- R/affichage_general.R
+++ R/affichage_general.R
@@ -5,18 +5,24 @@
 #' @param titre titre du graphique
 #' @param paletteinf to fix color on the scale
 #' @param palettesup to fix color on the scale
+#' @param mmechelle to set the two same scales TRUE/FALSE
 #'
 #' @return a plot
 #' @export
 #'
 #' @examples
 #' affichage_general(matrix(rnorm(100), nrow = 10), r =2)
-affichage_general <- function(Z, r, titre = "",paletteinf = "", palettesup = ""){
+affichage_general <- function(Z, r, titre = "",paletteinf = "", palettesup = "", mmechelle = FALSE){
   Y <- moving_average(Z,r)
   p1 <- plot_matrix(Z, titre =  "Z grid",paletteinf = paletteinf, palettesup = palettesup) + ggplot2::coord_cartesian(xlim = c(0, dim(Z)[1]), ylim = c(0, dim(Z)[2]))
-  p2 <- plot_matrix(Y,  r, titre = "Y grid",paletteinf = paletteinf, palettesup = palettesup)+ ggplot2::coord_cartesian(xlim = c(0, dim(Z)[1]), ylim = c(0, dim(Z)[2]))
-  p3 <- les_histogrammes(Z,Y, titre = "Histograms")
-  p4 <- les_boxplots(Z,Y)
+  if(mmechelle == FALSE){p2 <- plot_matrix(Y,  r, titre = "Y grid", paletteinf = paletteinf, palettesup = palettesup)+ ggplot2::coord_cartesian(xlim = c(0, dim(Z)[1]), ylim = c(0, dim(Z)[2]))}
+  else if(mmechelle == TRUE){
+    paletteinfY <- min(Z)
+    palettesupY <- max(Z)
+    p2 <- plot_matrix(Y,  r, titre = "Y grid", paletteinf = paletteinfY, palettesup = palettesupY)+ ggplot2::coord_cartesian(xlim = c(0, dim(Z)[1]), ylim = c(0, dim(Z)[2]))
+    }
+  p3 <- les_histogrammes(Z, Y, titre = "Histograms")
+  p4 <- les_boxplots(Z, Y)
 
   title <- cowplot::ggdraw() + cowplot::draw_label(titre, fontface='bold')
 
