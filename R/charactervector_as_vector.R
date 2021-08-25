65e26548ba48397ccf636b509d278f0348132a1a
Juliette Chiapello <chiapellojuliette@gmail.com>
2021-08-25 12:29:26 +0200

    now unsing regex and plotly

--- R/charactervector_as_vector.R
+++ R/charactervector_as_vector.R
@@ -1,6 +1,6 @@
 #' charactervector_as_vector
 #'
-#' @param x Ex : x <- "(0, 1)" a string
+#' @param chaine Ex : x <- "(0, 1)" a string
 #'
 #' @return a vector Ex : c(0, 1)
 #' @export
@@ -8,51 +8,8 @@
 #' @examples
 #' charactervector_as_vector("(0, 1)")
 #'
-charactervector_as_vector <- function(x){
-  # chaine_splitee <- strsplit(x,"")
-  #
-  # return(c(as.numeric(chaine_splitee[[1]][2]),as.numeric(chaine_splitee[[1]][5])))}
-x <- strsplit(x,"")
-x <- as.vector(x[[1]])
-x <- as.numeric(x)
-
-new <- c()
-
-if (length(x) != 1){
-for (i in (1:(length(x)-1))){
-  if (is.na(x[i]) & is.na(x[i+1]) ){
-     x[i] <- "temp"
-   }
-  }
-
-remove <- c("temp")
-sortie <- c()
-for (i in 1:length(x)){
-  if (! x[i] %in% remove){
-    sortie <- append(sortie, x[i])
-  }
+charactervector_as_vector <- function(chaine){
+  res <- regmatches(chaine, gregexpr("[[:digit:]]+", chaine))
+  res <- as.numeric(unlist(res))
+return(res)
 }
-
-sortie
-for (i in 1:(length(sortie)-1)){
-  while (! (is.na(sortie[i]) || is.na(sortie[i+1]))){
-    sortie[i] <- stringr::str_c(sortie[i], sortie[i+1])
-    sortie[i+1] <- NA
-  }
-}
-
-# emplacementNa <- c()
-# for (i in 1:(length(sortie)-1)){
-#   if(is.na(sortie[i])){emplacementNa <- append(emplacementNa, i)}
-# }
-
-
-sortie
-
-
-sortie <- sortie[!is.na(sortie)]
-sortie <- as.numeric(sortie)}
-
-else{sortie <- as.numeric(x)}
-
-return(sortie)}
--- R/plot_actual_cov.R
+++ R/plot_actual_cov.R
@@ -1,4 +1,4 @@
-#' plot_actua_covarnce
+#' plot_actual_covariance
 #'
 #' @param Z a matrix
 #' @param les_rayons radius of the moving average (vector)
@@ -7,7 +7,7 @@
 #' @param xlabs set x legend
 #' @param x x parametre
 #' @param y y parametre
-#' @param rayon "Rayon_pixels" or "Rayon_km" (radius unit)
+#' @param rayon "Radius" or "Rayon_km" (radius unit)
 #' @param echelle scale
 #' @param max set x max
 #' @param maxy_sup set y max
@@ -20,30 +20,31 @@
 #'
 #' @examples
 #' plot_actual_cov(matrix(rnorm(1:50), nrow = 10), c(1,2), list(c(0,1), c(1,1)), max = 20)
-plot_actual_cov <- function(Z,les_rayons,les_directions,  ylabs = "", xlabs ="Distance between variables", x = "Distance_pixels", y = "Empirical_covariance", rayon = "Rayon_pixels",  echelle = 1,  max = "", maxy_sup = "", maxy_inf = "" , relier = TRUE,vecteur_directeur = TRUE){
+plot_actual_cov <- function(Z,les_rayons,les_directions,  ylabs = "", xlabs ="Distance between variables", x = "Distance", y = "Empirical_covariance", rayon = "Radius",  echelle = 1,  max = "", maxy_sup = "", maxy_inf = "" , relier = TRUE,vecteur_directeur = TRUE){
   df <- data_frame_actual_covariance(Z, les_rayons, les_directions, echelle)
+  colnames(df) <- c("Distance", "Distance_km", "Empirical_covariance", "Empirical_correlation", "Direction", "Radius", "Rayon_km")
 
   ################COVARIANCE###################@
   if(y == "Empirical_covariance"){
-    if(x == "Distance_pixels"){p <- ggplot(data = df, aes(x = Distance_pixels, y = Empirical_covariance))}
+    if(x == "Distance"){p <- ggplot(data = df, aes(x = Distance, y = Empirical_covariance))}
     if(x == "Distance_km"){p <- ggplot(data = df, aes(x = Distance_km, y = Empirical_covariance))}
-    if (rayon == "Rayon_pixels"){p <- p + geom_point(aes(group = interaction(Direction, Rayon_pixels), col = factor(Rayon_pixels)))}
+    if (rayon == "Radius"){p <- p + geom_point(aes(group = interaction(Direction, Radius), col = factor(Radius)))}
     if (rayon == "Rayon_km"){p <- p + geom_point(aes(shape=Direction, col = factor(Rayon_km)))}
 
     if(relier == TRUE){
-      if (rayon == "Rayon_pixels"){p <- p + geom_line(aes(group = interaction(Direction, Rayon_pixels), col = factor(Rayon_pixels)))}
-      if (rayon == "Rayon_km"){p <- p + geom_line(aes(group = interaction(Direction, Rayon_pixels), col = factor(Rayon_km)))}
+      if (rayon == "Radius"){p <- p + geom_line(aes(group = interaction(Direction, Radius), col = factor(Radius)))}
+      if (rayon == "Rayon_km"){p <- p + geom_line(aes(group = interaction(Direction, Radius), col = factor(Rayon_km)))}
     }
   }
   ###########CORRELATION###############
   if(y == "Empirical_correlation"){
-    if(x == "Distance_pixels"){p <- ggplot(data = df, aes(x = Distance_pixels, y = Empirical_correlation))}
+    if(x == "Distance"){p <- ggplot(data = df, aes(x = Distance, y = Empirical_correlation))}
     if(x == "Distance_km"){p <- ggplot(data = df, aes(x = Distance_km, y = Empirical_correlation))}
-    if (rayon == "Rayon_pixels"){p <- p + geom_point(aes(shape = Direction, col = factor(Rayon_pixels)))}
+    if (rayon == "Radius"){p <- p + geom_point(aes(shape = Direction, col = factor(Radius)))}
     if (rayon == "Rayon_km"){p <- p + geom_point(aes(shape = Direction, col = factor(Rayon_km)))}
 
     if(relier == TRUE){
-      if (rayon == "Rayon_pixels"){p <- p + geom_line(aes(shape=Direction, col = factor(Rayon_pixels)))}
+      if (rayon == "Radius"){p <- p + geom_line(aes(shape=Direction, col = factor(Radius)))}
       if (rayon == "Rayon_km"){p <- p + geom_line(aes(shape=Direction, col = factor(Rayon_km)))}
     }
   }
@@ -68,5 +69,8 @@ plot_actual_cov <- function(Z,les_rayons,les_directions,  ylabs = "", xlabs ="Di
        if(les_rayons == 0){
       p <- p + guides(col = "none")}
     }
-  return(p)
+  gg_ply <- plotly::ggplotly(p) |>
+    plotly::layout(legend = list(title=list(text="Radius r \n of the \n moving average's \n window"))) |>
+    plotly::layout(legend = list(orientation = "v", x = 0.6, y = 0.99))
+  return(gg_ply)
 }
