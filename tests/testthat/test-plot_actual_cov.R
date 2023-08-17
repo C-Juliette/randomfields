cd7752ca3605de2272a37cb4a11ffeffc1ed7026
C-Juliette <chiapellojuliette@gmail.com>
2023-08-17 21:30:05 +0200

    tests

--- tests/testthat/test-plot_actual_cov.R
+++ tests/testthat/test-plot_actual_cov.R
@@ -1,29 +1,29 @@
-test_that("typeof plot_actual_cov works", {
+test_that("typeof plot_actual_cov works 1", {
   expect_equal(typeof(plot_actual_cov(matrix(rnorm(10000, 0, 1), nrow = 100), c(1,2), list(c(0,1), c(1,1)), max = 20))[1], "list")
 })
 
 
-test_that("typeof plot_actual_cov works", {
+test_that("typeof plot_actual_cov works 2", {
   expect_equal(typeof(plot_actual_cov(matrix(rnorm(10000, 0, 1), nrow = 100), c(1,2), list(c(0,1), c(1,1)),
                                                                                            relier = TRUE, max = 20))[1], "list")
 })
 
-test_that("typeof plot_actual_cov works", {
+test_that("typeof plot_actual_cov works 3", {
   expect_equal(typeof(plot_actual_cov(matrix(rnorm(10000, 0, 1), nrow = 100), c(1,2), list(c(0,1), c(1,1)),
                                       x = "Distance", max = 20))[1], "list")
 })
 
-test_that("typeof plot_actual_cov works", {
+test_that("typeof plot_actual_cov works 4", {
   expect_equal(typeof(plot_actual_cov(matrix(rnorm(10000, 0, 1), nrow = 100), c(1,2), list(c(0,1), c(1,1)),
                                       x = "Distance_km", max = 20))[1], "list")
 })
 
-test_that("typeof plot_actual_cov works", {
+test_that("typeof plot_actual_cov works 5", {
   expect_equal(typeof(plot_actual_cov(matrix(rnorm(10000, 0, 1), nrow = 100), c(1,2), list(c(0,1), c(1,1)),
                                       rayon = "Radius", max = 20))[1], "list")
 })
 
-test_that("typeof plot_actual_cov works", {
+test_that("typeof plot_actual_cov works 6", {
   expect_equal(typeof(plot_actual_cov(matrix(rnorm(10000, 0, 1), nrow = 100), c(1,2), list(c(0,1), c(1,1)),
                                       rayon = "Radius_km", max = 20))[1], "list")
 })
